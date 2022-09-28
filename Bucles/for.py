7b5b2ab8d2af9b72dee44d49141aadea872670c0
Cristobal <cristobal18422@gmail.com>
2022-09-28 08:29:14 -0500

    Se agrego nueva informacion y un ejercicio practico

---
+++ Bucles/for.py
@@ -0,0 +1,14 @@
+
+
+# contador = 1 
+# print(contador)
+# while contador < 1000:
+#    contador += 1 
+#    print(contador)    
+
+
+
+for contador in range(1001):
+    print(contador)
+
+    
---
+++ Bucles/recorrer.py
@@ -0,0 +1,12 @@
+def run():
+    # nombre = input('Escribe aqui tu nombre: ')
+    # for letra in nombre:
+    #         print(letra)
+
+
+    frase = input('Escribe una linea de GOT aqui: ')
+    for caracter in frase: 
+        print(caracter.upper())
+
+if __name__ == '__main__':
+    run()
